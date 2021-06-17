a35d8e5b7539f6e24615db941c470afe11cde4b3
Safwan Shaheer <devorein00@gmail.com>
2021-06-17 21:35:23 +0600

    Added toggle state to show states of data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from "react-query";
 import './App.css';
 
 interface AppProps { }
 
-function App({ }: AppProps) {
+function Fetch() {
   const { data, error, isLoading, isError, isFetched, isFetching, isIdle, isSuccess } = useQuery<any, any>('first-request', () => {
-    return Promise.reject(5)
+    return new Promise((resolve) => setTimeout(() => resolve(Math.random()), 2000))
   });
+  return <div>
+    <div>
+      Data is {data}
+    </div>
+    <div>
+      {JSON.stringify({ isError, error, isLoading, isFetched, isFetching, isIdle, isSuccess }, null, 2)}
+    </div>
+  </div>
+}
+
+function App({ }: AppProps) {
+  const [isVisible, setIsVisible] = useState(true);
 
   return (
     <div className="App">
-      {JSON.stringify({ isError, isFetched, isFetching, isIdle, isSuccess }, null, 2)}
+      <button onClick={() => setIsVisible((visible) => !visible)}>Toggle Visibility</button>
+      {isVisible && <Fetch />}
     </div>
   );
 }
