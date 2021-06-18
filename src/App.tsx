905c33d7f6ab0b3fe2ac79afc6097f7a93bb96fc
Safwan Shaheer <devorein00@gmail.com>
2021-06-18 09:49:09 +0600

    Added mutation with variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
-import { useQuery } from 'react-query';
+import React from 'react';
+import { useMutation } from "react-query";
 import './App.css';
-import client from "./react-query-client";
+
+const timer = (duration: number, variables: Record<string, any>) => new Promise((resolve) => setTimeout(() => {
+  console.log(`Mutation Running with variables ${JSON.stringify(variables, null, 2)}`)
+  resolve('Data received')
+}, duration))
 
 function App() {
-  
+  const mutation = useMutation((variables: Record<string, any>) => timer(1000, variables), {
+    onSuccess(data) {
+      console.log(`Data: ${data}`)
+    },
+    onError(error) {
+      console.log(`An error occurred, ${error}`)
+    },
+    onSettled(data, error) {
+      console.log({ data, error })
+    }
+  });
+
+  async function callMutation() {
+    console.log("Calling mutation")
+    await mutation.mutateAsync({
+      input: {
+        name: 'Devorein'
+      }
+    });
+    console.log("Mutation ran")
+  }
+
   return (
-    <div className="App">
+    <div onClick={callMutation} className="App">
       Hello World
     </div>
   );
